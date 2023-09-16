65c90db3ea423cb799be698838fc5a6a1d567d94
Masud <ranamasud0701@gmail.com>
2023-09-17 00:02:13 +0600

    added the jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
+
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -13,6 +14,25 @@ app.use(express.json());
 
 
 
+const jwtVerify=(req,res,next)=>{
+   const authorization = req.headers.authorization;
+   
+   if(!authorization){
+     return res.status(401).send({error:true,message:'unauthorized access'})
+   }
+   const token = authorization.split(' ')[1];
+  
+   jwt.verify(token, process.env.JWT_TOKEN,(err, decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    req.decoded= decoded;
+    next();
+  });
+
+}
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DbUser}:${process.env.DbPassword}@cluster0.igjj82v.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -35,7 +55,12 @@ async function run() {
     const onlineCollection = client.db('OnlineLearning').collection('course');
     const myCourseCollection = client.db('OnlineLearning').collection('MyCourse'); const userCollection = client.db('OnlineLearning').collection('User');
 
-
+    app.post('/jwt',(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.JWT_TOKEN,{expiresIn:'1h'});
+      res.send({token})
+   })
+    
 
     app.post('/courses', async (req, res) => {
       const item = req.body;
@@ -53,15 +78,30 @@ async function run() {
 
     app.post('/user', async (req, res) => {
       const item = req.body;
+      const query = {email:item.email}
+      const users = await userCollection.findOne(query);
+      if(users){
+         return res.send({message:'user already exists'})
+      }
       const result = await userCollection.insertOne(item);
 
       res.send(result);
     })
+  
+
 
     app.get('/user',async(req,res)=>{
          const item = req.body;
          const result = await userCollection.find(item).toArray();
          res.send(result);
+    })
+
+    app.get('/user/admin/:email',async(req,res)=>{
+        const email = req.params.email;
+        const query = {email:email};
+        const user = await userCollection.findOne(query);
+        const result = {admin:user?.role == 'admin'};
+        res.send(result);
     })
 
     app.patch('/user/:id',async(req,res)=>{
@@ -109,7 +149,7 @@ async function run() {
     })
 
 
-    app.get('/mycourse', async (req, res) => {
+    app.get('/mycourse',jwtVerify, async (req, res) => {
       const email = req.query.email;
       const sort = req.query.sort;
       const options = {
@@ -117,14 +157,41 @@ async function run() {
         sort: { "courseFee": sort == 'asc' ? 1 : -1 },
 
       };
-
+     
+     
       if (!email) {
         res.send([]);
       }
+      const decodedemail = req.decoded.email;
+     
+      if(email !== decodedemail ){
+        return res.status(403).send({error:true,message:'forbbiden access'})
+      }
+      
+
       const query = { email: email }
 
       const result = await myCourseCollection.find(query, options).toArray();
       res.send(result);
+    })
+
+    app.patch('/updatecourse/:id',async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const user = req.body;
+        const updateDoc = {
+          $set: user,
+        }
+        const result = await onlineCollection.updateOne(filter,updateDoc);
+        res.send(result);
+         
+    })
+
+    app.delete('/deletecourse/:id',async(req,res)=>{
+       const id = req.params.id;
+       const filter = {_id: new ObjectId(id)};
+       const result = await onlineCollection.deleteOne(filter);
+       res.send(result);
     })
 
     app.delete('/delete/:id', async (req, res) => {
